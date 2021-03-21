21ef531c20430604c3d87b428ab54345879bfba6
draganglumac <dragan.glumac@gmail.com>
2021-03-21 17:41:22 +0000

    Exercise 2.1

--- src/test/kotlin/chapter2/exercises/Exercise_2_1.kt
+++ src/test/kotlin/chapter2/exercises/Exercise_2_1.kt
@@ -6,14 +6,23 @@ import kotlinx.collections.immutable.persistentMapOf
 
 class Exercise_2_1 : WordSpec({
     // tag::init[]
-    fun fib(i: Int): Int = TODO()
+    fun fib(i: Int): Int {
+        tailrec fun go(n: Int, p: Pair<Int, Int>): Pair<Int, Int> =
+            if (n <= 2) p
+            else go(n - 1, Pair(p.second, p.first + p.second))
+
+        return when {
+            (i <= 2) -> 1
+            else -> go(i, Pair(1, 1)).second
+        }
+    }
     // end::init[]
 
     /**
      * Re-enable the tests by removing the `!` prefix!
      */
     "fib" should {
-        "!return the nth fibonacci number" {
+        "return the nth fibonacci number" {
             persistentMapOf(
                 Pair(1, 1),
                 Pair(2, 1),
