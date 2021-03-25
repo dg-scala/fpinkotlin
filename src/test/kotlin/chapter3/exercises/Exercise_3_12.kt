e361469449ed396bcb35357d772718e94e7bf992
draganglumac <dragan.glumac@gmail.com>
2021-03-25 20:32:24 +0000

    Unsolved Exercise 3.12

--- src/test/kotlin/chapter3/exercises/Exercise_3_12.kt
+++ src/test/kotlin/chapter3/exercises/Exercise_3_12.kt
@@ -4,10 +4,14 @@ import chapter3.List
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
 
-// tag::init[]
-fun <A, B> foldLeftR(xs: List<A>, z: B, f: (B, A) -> B): B = TODO()
+import chapter3.Listing_3_11.foldRight
 
-fun <A, B> foldRightL(xs: List<A>, z: B, f: (A, B) -> B): B = TODO()
+// tag::init[]
+fun <A, B> foldLeftR(xs: List<A>, z: B, f: (B, A) -> B): B =
+    foldRight(xs, z, TODO()) // unsolved - look at Solution_3_12.kt demystified
+
+fun <A, B> foldRightL(xs: List<A>, z: B, f: (A, B) -> B): B =
+    foldLeft(xs, z, TODO()) // unsolved - look at Solution_3_12.kt demystified
 // end::init[]
 
 class Exercise_3_12 : WordSpec({
