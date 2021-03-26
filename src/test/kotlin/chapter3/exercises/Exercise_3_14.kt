889c9f8754e06468aeb3fc47f12af8d4a7313aaf
draganglumac <dragan.glumac@gmail.com>
2021-03-26 09:59:23 +0000

    Exercise 3.14

--- src/test/kotlin/chapter3/exercises/Exercise_3_14.kt
+++ src/test/kotlin/chapter3/exercises/Exercise_3_14.kt
@@ -1,18 +1,32 @@
 package chapter3.exercises
 
+import chapter3.Cons
 import chapter3.List
+import chapter3.Listing_3_11.foldRight
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
 
 // tag::init[]
-fun <A> concat(xxs: List<List<A>>): List<A> = TODO()
+fun <A> concat(xxs: List<List<A>>): List<A> =
+    foldRight(
+        xxs,
+        List.empty(),
+        { xs1: List<A>, xs2: List<A> ->
+            foldRight(xs1, xs2, { a, ls -> Cons(a, ls) }) // <=> append(xs1, xs2)
+        })
 
-fun <A> concat2(xxs: List<List<A>>): List<A> = TODO()
+fun <A> concat2(xxs: List<List<A>>): List<A> =
+    foldRight(
+        xxs,
+        List.empty(),
+        { xs1, xs2 ->
+            append(xs1, xs2)
+        })
 // end::init[]
 
 class Exercise_3_14 : WordSpec({
     "list concat" should {
-        "!concatenate a list of lists into a single list" {
+        "concatenate a list of lists into a single list" {
             concat(
                 List.of(
                     List.of(1, 2, 3),
