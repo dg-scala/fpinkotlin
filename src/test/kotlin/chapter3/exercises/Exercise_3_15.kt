cdb60845702d4be140403044b87d4cb8a9898df0
draganglumac <dragan.glumac@gmail.com>
2021-03-26 12:33:18 +0000

    Exercise 3.15

--- src/test/kotlin/chapter3/exercises/Exercise_3_15.kt
+++ src/test/kotlin/chapter3/exercises/Exercise_3_15.kt
@@ -1,16 +1,20 @@
 package chapter3.exercises
 
+import chapter3.Cons
 import chapter3.List
+import chapter3.Listing_3_11.foldRight
+import chapter3.Nil
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
 
 // tag::init[]
-fun increment(xs: List<Int>): List<Int> = TODO()
+fun increment(xs: List<Int>): List<Int> =
+    foldRight(xs, List.empty(), { x, xx -> Cons(x + 1, xx) })
 // end::init[]
 
 class Exercise_3_15 : WordSpec({
     "list increment" should {
-        "!add 1 to every element" {
+        "add 1 to every element" {
             increment(List.of(1, 2, 3, 4, 5)) shouldBe
                 List.of(2, 3, 4, 5, 6)
         }
