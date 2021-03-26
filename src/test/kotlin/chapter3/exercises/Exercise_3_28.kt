302809079707fc65a9e4d8c9ab506fab2991d3b7
draganglumac <dragan.glumac@gmail.com>
2021-03-26 20:44:56 +0000

    Exercise 3.28

--- src/test/kotlin/chapter3/exercises/Exercise_3_28.kt
+++ src/test/kotlin/chapter3/exercises/Exercise_3_28.kt
@@ -7,15 +7,25 @@ import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
 
 // tag::init[]
-fun <A, B> fold(ta: Tree<A>, l: (A) -> B, b: (B, B) -> B): B = TODO()
+fun <A, B> fold(ta: Tree<A>, l: (A) -> B, b: (B, B) -> B): B = when (ta) {
+    is Leaf -> l(ta.value)
+    is Branch -> b(fold(ta.left, l, b), fold(ta.right, l, b))
+}
 
-fun <A> sizeF(ta: Tree<A>): Int = TODO()
+fun <A> sizeF(ta: Tree<A>): Int =
+    fold(ta, { _ -> 1 }, { b1, b2 -> 1 + b1 + b2 })
 
-fun maximumF(ta: Tree<Int>): Int = TODO()
+fun maximumF(ta: Tree<Int>): Int =
+    fold(ta, { x -> x }, { b1, b2 -> maxOf(b1, b2) })
 
-fun <A> depthF(ta: Tree<A>): Int = TODO()
+fun <A> depthF(ta: Tree<A>): Int =
+    fold(ta, { 0 }, { b1, b2 -> maxOf(b1, b2) + 1 })
 
-fun <A, B> mapF(ta: Tree<A>, f: (A) -> B): Tree<B> = TODO()
+fun <A, B> mapF(ta: Tree<A>, f: (A) -> B): Tree<B> =
+    fold(
+        ta,
+        { a: A -> Leaf(f(a)) },
+        { b1: Tree<B>, b2: Tree<B> -> Branch(b1, b2) })
 // end::init[]
 
 class Exercise_3_28 : WordSpec({
@@ -34,19 +44,19 @@ class Exercise_3_28 : WordSpec({
                 )
             )
         )
-        "!generalise size" {
+        "generalise size" {
             sizeF(tree) shouldBe 15
         }
 
-        "!generalise maximum" {
+        "generalise maximum" {
             maximumF(tree) shouldBe 21
         }
 
-        "!generalise depth" {
+        "generalise depth" {
             depthF(tree) shouldBe 5
         }
 
-        "!generalise map" {
+        "generalise map" {
             mapF(tree) { it * 10 } shouldBe
                 Branch(
                     Branch(Leaf(10), Leaf(20)),
