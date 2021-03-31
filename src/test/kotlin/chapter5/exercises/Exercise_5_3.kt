9d553e3b480d6f1522a20bfd216b47080bbf97d6
draganglumac <dragan.glumac@gmail.com>
2021-03-31 10:37:27 +0100

    Excercise 5.3 with help from the solution.

--- src/test/kotlin/chapter5/exercises/Exercise_5_3.kt
+++ src/test/kotlin/chapter5/exercises/Exercise_5_3.kt
@@ -1,7 +1,13 @@
 package chapter5.exercises
 
 import chapter3.List
+import chapter3.Nil as NilL
+import chapter3.Cons as ConsL
+import chapter5.Cons
+import chapter5.Empty
 import chapter5.Stream
+import chapter5.Stream.Companion.cons
+import chapter5.Stream.Companion.empty
 import chapter5.solutions.toList
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
@@ -12,21 +18,27 @@ import io.kotlintest.specs.WordSpec
 class Exercise_5_3 : WordSpec({
 
     //tag::init[]
-    fun <A> Stream<A>.takeWhile(p: (A) -> Boolean): Stream<A> = TODO()
+    fun <A> Stream<A>.takeWhile(p: (A) -> Boolean): Stream<A> =
+            when (this) {
+                is Empty -> empty()
+                is Cons ->
+                    if (p(head())) cons(head, { tail().takeWhile(p) })
+                    else empty()
+            }
     //end::init[]
 
     "Stream.takeWhile" should {
-        "!return elements while the predicate evaluates true" {
+        "return elements while the predicate evaluates true" {
             val s = Stream.of(1, 2, 3, 4, 5)
             s.takeWhile { it < 4 }.toList() shouldBe
                 List.of(1, 2, 3)
         }
-        "!return all elements if predicate always evaluates true" {
+        "return all elements if predicate always evaluates true" {
             val s = Stream.of(1, 2, 3, 4, 5)
             s.takeWhile { true }.toList() shouldBe
                 List.of(1, 2, 3, 4, 5)
         }
-        "!return empty if predicate always evaluates false" {
+        "return empty if predicate always evaluates false" {
             val s = Stream.of(1, 2, 3, 4, 5)
             s.takeWhile { false }.toList() shouldBe
                 List.empty()
