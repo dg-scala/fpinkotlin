d8de1861800ecac104530e15058b5b66f4d07847
draganglumac <dragan.glumac@gmail.com>
2021-04-01 18:34:20 +0100

    Exercise 5.14 with a small correction from the solution.

--- src/test/kotlin/chapter5/exercises/Exercise_5_14.kt
+++ src/test/kotlin/chapter5/exercises/Exercise_5_14.kt
@@ -1,5 +1,9 @@
 package chapter5.exercises
 
+import chapter4.exercises.flatMap
+import chapter4.exercises.getOrElse
+import chapter4.exercises.map
+import chapter5.Boilerplate.isEmpty
 import chapter5.Stream
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
@@ -10,16 +14,19 @@ import io.kotlintest.specs.WordSpec
 class Exercise_5_14 : WordSpec({
 
     //tag::startswith[]
-    fun <A> Stream<A>.startsWith(that: Stream<A>): Boolean = TODO()
+    fun <A> Stream<A>.startsWith(that: Stream<A>): Boolean =
+        zipAll(that)
+            .takeWhile { !it.second.isEmpty() }
+            .forAll { it.first == it.second }
     //end::startswith[]
 
     "Stream.startsWith" should {
-        "!detect if one stream is a prefix of another" {
+        "detect if one stream is a prefix of another" {
             Stream.of(1, 2, 3).startsWith(
                 Stream.of(1, 2)
             ) shouldBe true
         }
-        "!detect if one stream is not a prefix of another" {
+        "detect if one stream is not a prefix of another" {
             Stream.of(1, 2, 3).startsWith(
                 Stream.of(2, 3)
             ) shouldBe false
