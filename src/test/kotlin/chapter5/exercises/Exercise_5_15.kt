17b15511c912cc45b97779da7dcc3ec49f9961bc
draganglumac <dragan.glumac@gmail.com>
2021-04-01 18:59:12 +0100

    Exercise 5.15 with a small correction from the solution.

--- src/test/kotlin/chapter5/exercises/Exercise_5_15.kt
+++ src/test/kotlin/chapter5/exercises/Exercise_5_15.kt
@@ -1,7 +1,11 @@
 package chapter5.exercises
 
 import chapter3.List
+import chapter4.None
+import chapter4.Some
+import chapter5.Cons
 import chapter5.Stream
+import chapter5.solutions.append
 import chapter5.solutions.toList
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
@@ -14,23 +18,32 @@ import chapter3.Nil as NilL
 class Exercise_5_15 : WordSpec({
 
     //tag::tails[]
-    fun <A> Stream<A>.tails(): Stream<Stream<A>> = TODO()
+    fun <A> Stream<A>.tails(): Stream<Stream<A>> =
+        unfold(this) {
+            when (it) {
+                is Cons -> Some(Pair(it, it.tail()))
+                else -> None
+            }
+        }.append { Stream.empty() }
     //end::tails[]
 
-    fun <A, B> List<A>.map(f: (A) -> B): List<B> = TODO()
+    fun <A, B> List<A>.map(f: (A) -> B): List<B> = when (this) {
+        is ConsL -> ConsL(f(this.head), this.tail.map(f))
+        is NilL -> NilL
+    }
 
     "Stream.tails" should {
-        "!return the stream of suffixes of the input sequence" {
+        "return the stream of suffixes of the input sequence" {
             Stream.of(1, 2, 3)
                 .tails()
                 .toList()
                 .map { it.toList() } shouldBe
-                List.of(
-                    ConsL(1, ConsL(2, ConsL(3, NilL))),
-                    ConsL(2, ConsL(3, NilL)),
-                    ConsL(3, NilL),
-                    NilL
-                )
+                    List.of(
+                        ConsL(1, ConsL(2, ConsL(3, NilL))),
+                        ConsL(2, ConsL(3, NilL)),
+                        ConsL(3, NilL),
+                        NilL
+                    )
         }
     }
 })
