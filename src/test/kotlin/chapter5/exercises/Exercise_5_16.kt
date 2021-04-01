50feba8b78dcd9f051120ca21a689d5ee8d92f19
draganglumac <dragan.glumac@gmail.com>
2021-04-01 19:07:03 +0100

    Exercise 5.16, copy of the solution.

--- src/test/kotlin/chapter5/exercises/Exercise_5_16.kt
+++ src/test/kotlin/chapter5/exercises/Exercise_5_16.kt
@@ -1,6 +1,7 @@
 package chapter5.exercises
 
 import chapter3.List
+import chapter5.Boilerplate.foldRight
 import chapter5.Stream
 import chapter5.solutions.toList
 import io.kotlintest.shouldBe
@@ -13,11 +14,16 @@ class Exercise_5_16 : WordSpec({
 
     //tag::scanright[]
     fun <A, B> Stream<A>.scanRight(z: B, f: (A, () -> B) -> B): Stream<B> =
-        TODO()
+        foldRight({ Pair(z, Stream.of(z)) },
+            { a: A, p0: () -> Pair<B, Stream<B>> ->
+                val p1: Pair<B, Stream<B>> by lazy { p0() }
+                val b2: B = f(a) { p1.first }
+                Pair<B, Stream<B>>(b2, Stream.cons({ b2 }, { p1.second }))
+            }).second
     //end::scanright[]
 
     "Stream.scanRight" should {
-        "!behave like foldRight" {
+        "behave like foldRight" {
             Stream.of(1, 2, 3)
                 .scanRight(0, { a, b ->
                     a + b()
