e563944c33c4360bfc7c8080b4b8f7baf1ce7f9c
draganglumac <dragan.glumac@gmail.com>
2021-04-04 16:42:55 +0100

    Exercise 6.4

--- src/test/kotlin/chapter6/exercises/Exercise_6_4.kt
+++ src/test/kotlin/chapter6/exercises/Exercise_6_4.kt
@@ -1,6 +1,8 @@
 package chapter6.exercises
 
+import chapter3.Cons
 import chapter3.List
+import chapter3.Nil
 import chapter6.RNG
 import chapter6.rng1
 import io.kotlintest.shouldBe
@@ -12,11 +14,17 @@ import io.kotlintest.specs.WordSpec
 class Exercise_6_4 : WordSpec({
 
     //tag::init[]
-    fun ints(count: Int, rng: RNG): Pair<List<Int>, RNG> = TODO()
+    fun ints(count: Int, rng: RNG): Pair<List<Int>, RNG> {
+        return if (count > 0) {
+            val (i, r1) = nonNegativeInt(rng)
+            val (xs, r2) = ints(count - 1, r1)
+            Pair(Cons(i, xs), r2)
+        } else Pair(Nil, rng)
+    }
     //end::init[]
 
     "ints" should {
-        "!generate a list of ints of a specified length" {
+        "generate a list of ints of a specified length" {
 
             ints(5, rng1) shouldBe
                 Pair(List.of(1, 1, 1, 1, 1), rng1)
